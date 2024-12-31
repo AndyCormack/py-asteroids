e95434d6f9cfc0b91eb89551163bf02d8e4bdd6e
AndyCormack <incurse@gmail.com>
2024-12-31 18:58:21 +0000

    feat: main loop render window

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # Player input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Game state
+
+        # Rendering
+        screen.fill(pygame.Color(0,0,0))
+        # Render stuff
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
